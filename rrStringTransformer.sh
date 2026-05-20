c634497e4be188ac03e1ee7c5fbe92c8f8ec7ecc
CharlesPublicRec <cdbutle@outlook.com>
2026-05-20 11:04:03 -0500

    printf wip

--- rrStringTransformer.sh
+++ rrStringTransformer.sh
@@ -13,7 +13,20 @@ USAGE="Usage: command -ihv args"
 
 STRING="id, name, email, type(id, name, customFields(c1, c2, c3)), externalId"
 echo Given: $STRING 
+# Transform into JSON like format:
+# - id
+# - name
+# - email
+# - type / contains 3 subfield
+#   - id
+#   - name
+#   - customFields / contains 3 subfields
+#     - c1
+#     - c2
+#     - c3
+# - externalId
+
 echo 'return formatted version like JSON'
-printf "$STRING" | tr ',' '\n' | sed 's/^ *//'
+printf "$STRING" | tr ',' '\n' | tr ')' ' ' | tr '(' '\n\t' | sed 's/^ *//' | sed 's/^/- /'
 # -----------------------------------------------------------------
 # Shell script Common template from https://stackoverflow.com/questions/14008125/shell-script-common-template

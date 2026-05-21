c783ba5a8f030b295a56d8215b0a4b9000c54a73
CharlesPublicRec <cdbutle@outlook.com>
2026-05-20 19:03:55 -0500

    reverse sorted order args params

--- rrStringTransformer.py
+++ rrStringTransformer.py
@@ -3,8 +3,9 @@ import argparse
 STRING= "id, name, email, type(id, name, customFields(c1, c2, c3)), externalId"
 STRING_INPUT=[]
 
-argparse.ArgumentParser.add_argument('d', '--desc', action='store_true', help='Sorts in Descending (Z-A) order Defaults to Asc (A-Z)')
-args= argparse.ArgumentParser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--desc', action='store_true', help='Sorts in Descending (Z-A) order Defaults to Asc (A-Z)')
+args= parser.parse_args()
 desc= args.desc
 
 print(f"Given '({STRING})'")
@@ -39,7 +40,7 @@ def transform_string(s):
 
     return result
 
-def split_then_sort(usr_str):
+def split_then_sort(usr_str, desc=False):
     items = []
     current_item = ""
     n_level = 0
@@ -71,10 +72,10 @@ def split_then_sort(usr_str):
             items[i] = f"{item_name}({sorted_nested_item})"
 
     #Sort & Return lines
-    items.sort()
+    items.sort(reverse=desc)
     return ", ".join(items)
 
-second_str=split_then_sort(STRING)
+second_str=split_then_sort(STRING, desc)
 # print(f"split & Sort: {split_then_sort(STRING)}")
 
 originalString=STRING
@@ -84,7 +85,10 @@ for nestedLevel, data in transform_string(originalString):
     indent = "  " * nestedLevel
     print(f"{indent}- {data}")
     
-print(f"\nSorted Output:")
+if desc:
+    print(f"\nSorted Output in Descending Order:")
+else:
+    print(f"\nSorted Output in Ascending Order:")
 #Second Output
 for nestedLevel, data in transform_string(second_str):
     indent = "  " * nestedLevel
